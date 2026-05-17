564e19923c6cd8028bd5dfb0fc6141f19bfacfab
Antares <antares.mi@outlook.com>
2026-05-17 14:18:28 +0800

    feat: hide domain in search result if user has no permission to view

--- common/config.ts
+++ common/config.ts
@@ -3,11 +3,17 @@ import { Schema } from "hydrooj";
 export const enum CE_ConfigKey {
     ForceSystemDisplayName = "force-system-display-name",
     DisableUserEditDisplayname = "disable-user-edit-displayname",
+    HideNoPermDomainInSearchResult = "hide-no-perm-domain-in-search-result",
 }
 
 export const Config = Schema.object({
-    [CE_ConfigKey.ForceSystemDisplayName]: Schema.boolean().default(false),
-    [CE_ConfigKey.DisableUserEditDisplayname]: Schema.boolean().default(false),
+    [CE_ConfigKey.ForceSystemDisplayName]: Schema.boolean().description("Force system display name").default(false),
+    [CE_ConfigKey.DisableUserEditDisplayname]: Schema.boolean()
+        .description("Disable user edit display name")
+        .default(false),
+    [CE_ConfigKey.HideNoPermDomainInSearchResult]: Schema.boolean()
+        .description("Hide domain in search result if user has no permission to view")
+        .default(false),
 }).description("Domain Extension Settings");
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
