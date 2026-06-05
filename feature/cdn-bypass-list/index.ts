e98f2618dab2ff384ae991e76326fde0631cea50
Antares <antares.mi@outlook.com>
2026-06-05 23:49:34 +0800

    feat: bypass cdn

---
+++ feature/cdn-bypass-list/index.ts
@@ -0,0 +1,20 @@
+import { type Context, type Handler } from "hydrooj";
+import ip from "ip";
+import isCidr from "is-cidr";
+
+import { CE_ConfigKey, getSettingKeys } from "../../common/config";
+
+export function applyCdnBypass(ctx: Context) {
+    ctx.on("handler/before", (handler: Handler) => {
+        const bypassList = (ctx.setting.get(getSettingKeys(CE_ConfigKey.CdnBypass)) as string[]) ?? [];
+
+        const ipAddress = handler.request.ip;
+        const shouldBypass = bypassList.some(
+            (bypass) => (isCidr(bypass) && ip.cidrSubnet(bypass).contains(ipAddress)) || ip.isEqual(ipAddress, bypass),
+        );
+
+        if (shouldBypass) {
+            handler.UiContext.cdn_prefix = "/";
+        }
+    });
+}
