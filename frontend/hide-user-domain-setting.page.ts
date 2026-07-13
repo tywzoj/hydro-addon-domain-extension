6bdcd674aea0d6657938aa42dd9acfcf4603a6ba
Antares <antares.mi@outlook.com>
2026-07-14 00:02:16 +0800

    fix

--- frontend/hide-user-domain-setting.page.ts
+++ frontend/hide-user-domain-setting.page.ts
@@ -2,7 +2,7 @@ import { $, addPage, AutoloadPage, NamedPage } from "@hydrooj/ui-default";
 
 addPage(
     new AutoloadPage("hide-user-domain-setting", () => {
-        if (UiContext.hideUserDomainSetting) {
+        if (UserContext && UserContext._id && UiContext.hideUserDomainSetting) {
             const $menuItem = $(".nav__dropdown li.menu__item").has('a[href*="/home/settings/domain"]');
             $menuItem.next(".menu__seperator").remove();
             $menuItem.remove();
@@ -12,7 +12,7 @@ addPage(
 
 addPage(
     new NamedPage([], () => {
-        if (UiContext.hideUserDomainSetting) {
+        if (UserContext && UserContext._id && UiContext.hideUserDomainSetting) {
             $(".menu li.menu__item").has('a[href*="/home/settings/domain"]').remove();
         }
     }),
