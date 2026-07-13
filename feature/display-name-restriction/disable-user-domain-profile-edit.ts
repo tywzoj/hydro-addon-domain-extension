69c6ce4a0831ea776030fdcefec39ae8dd95613e
Antares <antares.mi@outlook.com>
2026-07-14 00:00:05 +0800

    hide entry

--- feature/display-name-restriction/disable-user-domain-profile-edit.ts
+++ feature/display-name-restriction/disable-user-domain-profile-edit.ts
@@ -4,15 +4,27 @@ import { PRIV, PrivilegeError } from "hydrooj";
 import { CE_ConfigKey, getSettingKeys } from "../../common/config";
 import { CE_String } from "./i18n";
 
+declare module "hydrooj" {
+    export interface UiContext {
+        hideUserDomainSetting?: boolean;
+    }
+}
+
 export function applyDisableUserDomainProfileEdit(ctx: Context) {
+    const checkDomainSettingDisabled = (handler: Handler): boolean => {
+        return (
+            (ctx.setting.get(getSettingKeys(CE_ConfigKey.DisableUserEditDisplayname)) as boolean) &&
+            (handler.domain?._id !== "system" || !handler.user.hasPriv(PRIV.PRIV_EDIT_SYSTEM))
+        );
+    };
+
+    ctx.on("handler/before", (handler) => {
+        handler.UiContext.hideUserDomainSetting = checkDomainSettingDisabled(handler);
+    });
+
     ctx.on("handler/before/HomeSettings", (handler: Handler) => {
-        if (
-            handler.args.category === "domain" &&
-            ctx.setting.get(getSettingKeys(CE_ConfigKey.DisableUserEditDisplayname))
-        ) {
-            if (handler.domain?._id !== "system" || !handler.user.hasPriv(PRIV.PRIV_EDIT_SYSTEM)) {
-                throw new PrivilegeError(CE_String.EditDomainProfile);
-            }
+        if (handler.args.category === "domain" && checkDomainSettingDisabled(handler)) {
+            throw new PrivilegeError(CE_String.EditDomainProfile);
         }
     });
 }
---
+++ frontend/hide-user-domain-setting.page.ts
@@ -0,0 +1,19 @@
+import { $, addPage, AutoloadPage, NamedPage } from "@hydrooj/ui-default";
+
+addPage(
+    new AutoloadPage("hide-user-domain-setting", () => {
+        if (UiContext.hideUserDomainSetting) {
+            const $menuItem = $(".nav__dropdown li.menu__item").has('a[href*="/home/settings/domain"]');
+            $menuItem.next(".menu__seperator").remove();
+            $menuItem.remove();
+        }
+    }),
+);
+
+addPage(
+    new NamedPage([], () => {
+        if (UiContext.hideUserDomainSetting) {
+            $(".menu li.menu__item").has('a[href*="/home/settings/domain"]').remove();
+        }
+    }),
+);
